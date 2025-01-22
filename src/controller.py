Fixed minor mistake in variance calculation. Probably more where that came from. Also changed some plotting stuff

src/controller.py
@@ -808,7 +808,7 @@ class Controller():
 
         # Transform from hourly to quarter hourly basis
         # Scale from percentage based schedule to light intensity
-        u_base = 250/100*np.repeat(light_schedule, 4)     
+        u_base = self.model.C_PPFD_max/100*np.repeat(light_schedule, 4)     
 
         assert len(u_base) >= self.N, f"Imported light schedule too short. Len: {len(u_base)}, N: {N}"
 

src/plotter.py
@@ -45,12 +45,39 @@ class Plotter():
 
         bidding_runs = [run for run in controller.optimization_results['runs'] if 'bidding result' in controller.optimization_results['runs'][run]]
 
-        # BEGIN PLOTTING (or more like saving plots, but you get it)
+        # BEGIN PLOTTING
         ##################################################
         
         if self.controller.model.title == "Vertical Farm" or self.controller.model.title == "Gjermund plant model":
 
+            
+            ######################################################
+
+            plt.figure(figsize=config.plot_format)
+            
+
             for run_name in controller.optimization_results['runs']:
+
+                if(run_name in bidding_runs and 'Realized' not in run_name):
+
+                    bid_ts  = self.controller.optimization_results['runs'][run_name]['timeseries']
+                    bid_volume_up   = bid_ts['P_up']    # Volume up
+                    bid_volume_dn   = bid_ts['P_dn']    # Volume down
+                    bid_price_up    = bid_ts['C_up']    # Price up
+                    bid_price_dn    = bid_ts['C_dn']    # Price down
+
+                    u_nom = controller.optimization_results['runs'][run_name]['timeseries']['u_nom']
+                    x_bid = controller.optimization_results['runs'][run_name]['timeseries']['x']
+                    # fw_interval = calculate_freshweight_interval(controller, u_bid, b_p_up, b_p_dn, b_c_up, b_c_dn)
+                    fw_variance = propagate_process_covariance(controller, x_bid, u_nom, bid_volume_up, bid_volume_dn, bid_price_up, bid_price_dn)
+                    fw_sd = np.sqrt(fw_variance)
+                    fw = np.array(controller.model.freshweight(x_bid[:,1:])).flatten()
+
+                    fw_ub = fw + 1.96*fw_sd
+                    fw_lb = fw - 1.96*fw_sd
+
+                    plt.fill_between(t, fw_lb, fw_ub, color='green', alpha=0.2)
+
                 x = controller.optimization_results['runs'][run_name]['timeseries']['x']
                 x_fw = controller.model.freshweight(x[:,1:])
                 plt.plot(t, x_fw, label=f"{run_name} (g/plant)")
@@ -60,6 +87,9 @@ class Plotter():
             plt.xlabel("Time (days)")
             plt.legend(loc="upper left")
             plt.title(f"Expected freshweight of plant growth (g/plant). ({controller.market.date}, {controller.market.bidding_zone})")
+
+
+
 
         if self.controller.model.title == "Battery":
 
@@ -223,32 +253,6 @@ class Plotter():
             plt.savefig(config.plot_path + foldername + "/" + filename + "." + config.plot_file_type, format=config.plot_file_type)
 
 
-            ######################################################
-
-            plt.figure(figsize=config.plot_format)
-            
-            u_bid = controller.optimization_results['runs'][run]['timeseries']['u']
-            x_bid = controller.optimization_results['runs'][run]['timeseries']['x']
-            # fw_interval = calculate_freshweight_interval(controller, u_bid, b_p_up, b_p_dn, b_c_up, b_c_dn)
-            fw_variance = propagate_process_covariance(controller, x_bid, u_bid, bid_volume_up, bid_volume_dn, bid_price_up, bid_price_dn)
-            fw_sd = np.sqrt(fw_variance)
-            fw = np.array(controller.model.freshweight(x_bid[:,1:])).flatten()
-
-            fw_ub = fw + 1.96*fw_sd
-            fw_lb = fw - 1.96*fw_sd
-
-            # plt.fill_between(t, fw-3*fw_sd, fw+3*fw_sd, color='green', alpha=0.2)
-            # plt.fill_between(t, fw-2*fw_sd, fw+2*fw_sd, color='green', alpha=0.2)
-            plt.fill_between(t, fw_lb, fw_ub, color='green', alpha=0.6)
-            plt.axhline(y=controller.model.Final_fw_sht, color='gray', linestyle=':', label="Required Freshweight (g/plant)")
-            plt.ylabel("Fresh weight (g/plant)")
-            plt.xlabel("Time (days)")
-            plt.title(f'{run}: 95% confidence interval of freshweight throughout one growth cycle')
-
-            filename = f"{run_sanitized}_freshweight_variance"
-            plt.savefig(config.plot_path + foldername + "/" + filename + "." + config.plot_file_type, format=config.plot_file_type)
-
-
             #################################################################################
             # Bidding volumes and activation chances
 
@@ -316,22 +320,39 @@ class Plotter():
         ##################################################
         plt.figure(figsize=config.plot_format)
 
+        bidding_runs = [run for run in controller.optimization_results['runs'] if 'bidding result' in controller.optimization_results['runs'][run]]
+
+        for run_name in bidding_runs:
+            if 'Realized' in run_name:
+                continue 
+            bid_ts  = self.controller.optimization_results['runs'][run_name]['timeseries']
+            bid_volume_up   = bid_ts['P_up']    # Volume up
+            bid_volume_dn   = bid_ts['P_dn']    # Volume down
+            bid_price_up    = bid_ts['C_up']    # Price up
+            bid_price_dn    = bid_ts['C_dn']    # Price down
+
+            u_nom = controller.optimization_results['runs'][run_name]['timeseries']['u_nom']
+            x_bid = controller.optimization_results['runs'][run_name]['timeseries']['x']
+            # fw_interval = calculate_freshweight_interval(controller, u_bid, b_p_up, b_p_dn, b_c_up, b_c_dn)
+            fw_variance = propagate_process_covariance(controller, x_bid, u_nom, bid_volume_up, bid_volume_dn, bid_price_up, bid_price_dn)
+            fw_sd = np.sqrt(fw_variance)
+            fw = np.array(controller.model.freshweight(x_bid[:,1:])).flatten()
+
+            fw_ub = fw + 1.96*fw_sd
+            fw_lb = fw - 1.96*fw_sd
+
+            plt.fill_between(t, fw_lb, fw_ub, color='green', alpha=0.4)
+
         for case in range(freshwewights.shape[0]):
-            plt.plot(t, freshwewights[case,:], color='green', alpha=0.2)
+            plt.plot(t, freshwewights[case,:], color='blue', alpha=0.2)
         plt.axhline(y=controller.model.Final_fw_sht, color='gray', linestyle=':', label="Required Freshweight (g/plant)")
         plt.ylabel("Fresh weight (g/plant)")
         plt.xlabel("Time (days)")
         plt.title(f'Simulated {m} different cases of plausible activations')
         # plt.legend()
 
-
         filename = "random_activations"
         plt.savefig(config.plot_path + foldername + "/" + filename + "." + config.plot_file_type, format=config.plot_file_type)
-
-
-
-
-
 
     def plot_spot_mfrr_prices(self):
 

src/simulator.py
@@ -66,6 +66,7 @@ class Simulator():
         N = controller.N
         dt = controller.dt
         seed = None     # Set to None for random 
+        F = controller.model.casadi_function_fe()
 
         u_base = controller.optimization_results['runs']['Baseline']['timeseries']['u']
 
@@ -101,7 +102,7 @@ class Simulator():
             for k in range(N):
                 #Forward euler
                 # X[:,k+1] = X[:,k] + dt*np.array(controller.model.derivative(X[:,k], np.array([u[k]]))).reshape(1, -1)
-                X[:,k+1] = np.array(controller.model.casadi_function()(X[:,k], np.array([u[k]]))).reshape(1, -1)
+                X[:,k+1] = np.array(F(X[:,k], np.array([u[k]]))).reshape(1, -1)
 
             fw = controller.model.freshweight(X)
         

src/utils.py
@@ -633,18 +633,18 @@ def propagate_process_covariance(controller, x_bid, u_bid, bidding_volumes_up, b
     for k in range(N):
         # Input uncertainty terms
         a, b = 1000*bidding_volumes_up[k]/model.C_conv_PPFD, 1000*bidding_volumes_dn[k]/model.C_conv_PPFD
-        p_a = market.Pr_a_up(spot_prices[k], bidding_prices_up[k])
-        p_b = market.Pr_a_dn(spot_prices[k], bidding_prices_dn[k])
+        p_up = market.Pr_a_up(spot_prices[k], bidding_prices_up[k])
+        p_dn = market.Pr_a_dn(spot_prices[k], bidding_prices_dn[k])
 
         # Expected values of u_a and u_b
-        E_u_a = a * p_a
-        E_u_b = b * p_b
-        E_u = u_bid[k] + E_u_a + E_u_b
+        E_u_up = a * p_up
+        E_u_dn = b * p_dn
+        E_u = u_bid[k] - E_u_up + E_u_dn
 
         # Variance of u
-        Var_u_a = a**2 * p_a * (1 - p_a)
-        Var_u_b = b**2 * p_b * (1 - p_b)
-        Var_u = Var_u_a + Var_u_b - 2 * (E_u_a * E_u_b)
+        Var_u_a = a**2 * p_up * (1 - p_up)
+        Var_u_b = b**2 * p_dn * (1 - p_dn)
+        Var_u = Var_u_a + Var_u_b - 2 * (E_u_up * E_u_dn)
 
         # # Skewness of u
         # Skew_u_a = -((1 - 2*p_a) / (np.sqrt(Var_u_a) if Var_u_a > 0 else 1e-6) if Var_u_b>1 else 0)