Fixed bug with mfrr_opt. Fixed activation estimation and earnings calculation

src/controller.py
@@ -790,7 +790,7 @@ class Controller():
 
         if A is None:
             Activations = np.vstack((self.market.activation_prob_up(self.spot_prices, Bids[2,:]),
-                                     self.market.activation_prob_up(self.spot_prices, Bids[3,:])))
+                                     self.market.activation_prob_down(self.spot_prices, Bids[3,:])))
         else:
             Activations = A
         
@@ -1191,7 +1191,7 @@ class Controller():
             ['Costs'] + costs,
             ['Earnings'] + earnings,
             ['Totals'] + totals,
-            ['Total percentage cost reduction'] + cost_reduction_percent,
+            ['Cost reduction (%)'] + cost_reduction_percent,
         ]
 
         cost_table = generate_table(cost_data, header=[run for run in self.optimization_results['runs']])

src/market.py
@@ -237,14 +237,15 @@ class Market:
                     price_data      = AM_data[[f'{zone} Spot Price', f'{zone} {direction} Price']]
                 
                 activation_data = AM_data[[f'{zone} Spot Price', f'{zone} Activated {direction} Volume']]
+                activation_data[f'{zone} Activated {direction} Volume'] = (activation_data[f"{zone} Activated {direction} Volume"] > 0).astype(int)
 
                 price_statistics[zone][direction]['means']      = np.mean(np.array(price_data), axis=0)
                 price_statistics[zone][direction]['cov']        = np.cov(price_data.T)
                 activation_statistics[zone][direction]['means'] = np.mean(np.array(activation_data), axis=0)
                 activation_statistics[zone][direction]['cov']   = np.cov(activation_data.T)
 
-        self.AM_price_stats = price_statistics
-        self.AM_activation_stats = price_statistics
+        self.AM_price_stats         = price_statistics
+        self.AM_activation_stats    = activation_statistics
 
         return 0
     
@@ -285,6 +286,7 @@ class Market:
                     price_data      = CM_data[[f'{zone} Spot Price', f'{zone} {direction} Price']]
                 
                 activation_data = CM_data[[f'{zone} Spot Price', f'{zone} {direction} Volume procured']]
+                activation_data[f'{zone} {direction} Volume procured'] = (activation_data[f'{zone} {direction} Volume procured'] > 0).astype(int)
                 
                 price_statistics[zone][direction]['means']      = np.mean(np.array(price_data), axis=0)
                 price_statistics[zone][direction]['cov']        = np.cov(np.array(price_data).reshape((2,-1)))
@@ -292,7 +294,7 @@ class Market:
                 reservation_statistics[zone][direction]['cov']   = np.cov(np.array(activation_data).reshape((2,-1)))
 
         self.CM_price_stats = price_statistics
-        self.CM_activation_stats = price_statistics
+        self.CM_activation_stats = reservation_statistics
 
         return 0
     